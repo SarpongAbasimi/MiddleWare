206a601f5798ff20386012ba7e720a063fb6ec80
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-05-23 15:45:15 +0100

    [SA] Addded locgback as a dependency and loggings to application

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 - Experimenting with http4s middleware and logs
 - Middleware and logs
+- [Useful Docs for logging](https://engineering.footballradar.com/introduction-to-logging-in-scala/)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,31 +5,38 @@ import cats.effect.{ExitCode, IO, IOApp, Sync}
 import org.http4s.dsl.impl.Root
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.implicits._
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContext.global
 
 object Main extends IOApp {
 
-  val helloWorldService: Kleisli[IO, Request[IO], Response[IO]] = HttpRoutes.of[IO] {
-    case GET -> Root =>
+  val helloWorldService: Kleisli[IO, Request[IO], Response[IO]] = HttpRoutes
+    .of[IO] { case GET -> Root =>
       Ok(s"http4s")
-  }.orNotFound
-
-  def middlewareService(service: Kleisli[IO, Request[IO], Response[IO]])(implicit sync: Sync[IO]) = Kleisli { (req: Request[IO]) =>
-    service(req).map{
-      case Status.Successful(res) => {
-        res.putHeaders(Header("company", "47Deg"))
-      }
-      case res => res
     }
-  }
+    .orNotFound
 
-  def run(args: List[String]): IO[ExitCode] =
-    BlazeServerBuilder[IO](global)
-      .bindHttp(8080, "localhost")
-      .withHttpApp(middlewareService(helloWorldService))
-      .serve
-      .compile
-      .drain
-      .as(ExitCode.Success)
+  def middlewareService(service: Kleisli[IO, Request[IO], Response[IO]])(implicit sync: Sync[IO]) =
+    Kleisli { (req: Request[IO]) =>
+      service(req).map {
+        case Status.Successful(res) =>
+          res.putHeaders(Header("company", "47Deg"))
+        case res => res
+      }
+    }
+
+  def run(args: List[String]): IO[ExitCode] = {
+    val logger = LoggerFactory.getLogger("Main")
+    for {
+      _ <- Sync[IO].delay(logger.info("stating server . . ."))
+      server <- BlazeServerBuilder[IO](global)
+        .bindHttp(8080, "localhost")
+        .withHttpApp(middlewareService(helloWorldService))
+        .serve
+        .compile
+        .drain
+        .as(ExitCode.Success)
+    } yield server
+  }
 }
